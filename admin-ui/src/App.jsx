f39a6fa18078edd4ac3f1bb654c745b9a0174454
Fergus3763 <fergus@email4us.com>
2025-11-05 17:41:25 +0000

    feat(dashboard): add AV route to Admin UI

--- admin-ui/src/App.jsx
+++ admin-ui/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import VenueSetup from "./pages/Dashboard/VenueSetup/index.jsx";
 import Rooms from "./pages/Dashboard/Rooms/index.jsx";
 import FoodBeverage from "./pages/Dashboard/FoodBeverage/index.jsx";
+import AV from "./pages/Dashboard/AV/index.jsx";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/admin/venue" element={<VenueSetup />} />
         <Route path="/admin/rooms" element={<Rooms />} />
         <Route path="/admin/fnb" element={<FoodBeverage />} />
+        <Route path="/admin/av" element={<AV />} />
       </Routes>
     </BrowserRouter>
   );
