5b1862fa8c0d8a84319f28d675e574180a4badc6
Fergus3763 <fergus@email4us.com>
2025-11-20 15:06:02 +0000

    Create RoomOverview index.jsx

---
+++ admin-ui/src/pages/RoomOverview/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function RoomOverview() {
+  return (
+    <div>
+      <h1>Room Overview</h1>
+      <p>
+        This page will become a side-by-side comparison view for all rooms and meeting
+        spaces.
+      </p>
+      <p>
+        It will show which inclusions and add-ons apply to each room, along with key
+        details like capacity and any special rules.
+      </p>
+      <p>
+        For now this is only a placeholder so hotel admins know that a consolidated
+        overview is planned for a future version.
+      </p>
+    </div>
+  );
+}
